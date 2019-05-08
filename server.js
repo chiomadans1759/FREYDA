16dd994901da16eca4598075bf58009046c1475e
Otse Obande <otseobande@gmail.com>
2019-05-08 12:39:11 +0100

    serve index.html for all server requests

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 app = express();
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'dist/index.html')));
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8000;
 app.listen(port);
