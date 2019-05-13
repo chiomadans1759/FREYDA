45fb987934af03251320f94411854517c61c7025
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2019-05-13 13:02:14 +0100

    Auth layout done

--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -1,25 +1,15 @@
 <template>
   <main class="admin-layout">
-    <div class="sidebar-left">
-      <sidebar-left/>
-    </div>
-    <div class="main-area">
-      <router-view/>
-    </div>
-    <div class="sidebar-right">
-      <sidebar-right/>
-    </div>
+    
   </main>
 </template>
 
 <script>
-import SidebarLeft from "@/components/SidebarLeft.vue";
-import SidebarRight from "@/components/SidebarRight.vue";
+import AdminSidebar from "@/components/AdminSidebar"
 
 export default {
   components: {
-    SidebarLeft,
-    SidebarRight
+    AdminSidebar
   }
 };
 </script>
