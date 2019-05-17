06f10c63892fa4a4ac376911613b0385362ac40a
filigreti <aolfiligre@gmail.com>
2019-05-17 11:42:52 +0100

    ft-finished-highlight

--- src/views/Admin/Dashboard.vue
+++ src/views/Admin/Dashboard.vue
@@ -1,241 +1,241 @@
 <template>
-	<main class="dashboard py-3">
-		<div class="container">
-			<div class="row mb-3 stat-cards">
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">Capital Commited</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>$2.0BN</b>
-							</h3>
-							<p>USD</p>
-						</div>
-					</div>
-				</div>
+  <main class="dashboard py-3">
+    <div class="container">
+      <div class="row mb-3 stat-cards">
+        <div class="col">
+          <div ref="card" class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">Capital Commited</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>$2.0BN</b>
+              </h3>
+              <p>USD</p>
+            </div>
+          </div>
+        </div>
 
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">Residual Value</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>$113M</b>
-							</h3>
-							<p>USD</p>
-						</div>
-					</div>
-				</div>
+        <div class="col">
+          <div class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">Residual Value</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>$113M</b>
+              </h3>
+              <p>USD</p>
+            </div>
+          </div>
+        </div>
 
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">Capital Called</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>$1.6BN</b>
-							</h3>
-							<p>USD</p>
-						</div>
-					</div>
-				</div>
+        <div class="col">
+          <div class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">Capital Called</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>$1.6BN</b>
+              </h3>
+              <p>USD</p>
+            </div>
+          </div>
+        </div>
 
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">Capital Distributed</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>$2.2BN</b>
-							</h3>
-							<p>USD</p>
-						</div>
-					</div>
-				</div>
+        <div class="col">
+          <div class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">Capital Distributed</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>$2.2BN</b>
+              </h3>
+              <p>USD</p>
+            </div>
+          </div>
+        </div>
 
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">Current IRR</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>17.2%</b>
-							</h3>
-							<p>IRR</p>
-						</div>
-					</div>
-				</div>
-			</div>
+        <div class="col">
+          <div class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">Current IRR</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>17.2%</b>
+              </h3>
+              <p>IRR</p>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<div class="row mb-3 stat-cards">
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">Net Cash Flow</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>$628M</b>
-							</h3>
-							<p>USD</p>
-						</div>
-					</div>
-				</div>
+      <div class="row mb-3 stat-cards">
+        <div class="col">
+          <div class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">Net Cash Flow</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>$628M</b>
+              </h3>
+              <p>USD</p>
+            </div>
+          </div>
+        </div>
 
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">Distributed to paid-in Capital</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>$1.4x</b>
-							</h3>
-							<p>DPI</p>
-						</div>
-					</div>
-				</div>
+        <div class="col">
+          <div class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">Distributed to paid-in Capital</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>$1.4x</b>
+              </h3>
+              <p>DPI</p>
+            </div>
+          </div>
+        </div>
 
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">Residual value to paid-in Capital</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>0.1x</b>
-							</h3>
-							<p>RVPI</p>
-						</div>
-					</div>
-				</div>
+        <div class="col">
+          <div class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">Residual value to paid-in Capital</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>0.1x</b>
+              </h3>
+              <p>RVPI</p>
+            </div>
+          </div>
+        </div>
 
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">Total value to paid-in Capital</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>1.5x</b>
-							</h3>
-							<p>TVPI</p>
-						</div>
-					</div>
-				</div>
+        <div class="col">
+          <div class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">Total value to paid-in Capital</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>1.5x</b>
+              </h3>
+              <p>TVPI</p>
+            </div>
+          </div>
+        </div>
 
-				<div class="col">
-					<div class="card">
-						<div class="card-header text-center pb-1 px-0">
-							<p class="card-header-title mb-0">75th - 25th Percentile IRR</p>
-						</div>
-						<div class="card-body text-center pb-1">
-							<h3 class="mb-0">
-								<b>12.4%</b>
-							</h3>
-							<p>SPREAD</p>
-						</div>
-					</div>
-				</div>
-			</div>
+        <div class="col">
+          <div class="card" tabindex="1">
+            <div class="card-header text-center pb-1 px-0">
+              <p class="card-header-title mb-0">75th - 25th Percentile IRR</p>
+            </div>
+            <div class="card-body text-center pb-1">
+              <h3 class="mb-0">
+                <b>12.4%</b>
+              </h3>
+              <p>SPREAD</p>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<div class="row mb-3">
-				<div class="col">
-					<div class="card border-0 rounded-0">
-						<div class="card-body">
-							<div class="container">
-								<div class="d-flex">
-									<div class="col-2 mx-3">
-										<doughnut-chart />
-										<p class="text-center mt-3 mb-0">By Geography</p>
-									</div>
+      <div class="row mb-3">
+        <div class="col">
+          <div class="card border-0 rounded-0">
+            <div class="card-body">
+              <div class="container">
+                <div class="d-flex">
+                  <div class="col-2 mx-3">
+                    <doughnut-chart/>
+                    <p class="text-center mt-3 mb-0">By Geography</p>
+                  </div>
 
-									<div class="col-2 mx-3">
-										<doughnut-chart />
-										<p class="text-center mt-3 mb-0">By Strategy</p>
-									</div>
+                  <div class="col-2 mx-3">
+                    <doughnut-chart/>
+                    <p class="text-center mt-3 mb-0">By Strategy</p>
+                  </div>
 
-									<div class="col-2 mx-3">
-										<doughnut-chart />
-										<p class="text-center mt-3 mb-0">By Industry</p>
-									</div>
+                  <div class="col-2 mx-3">
+                    <doughnut-chart/>
+                    <p class="text-center mt-3 mb-0">By Industry</p>
+                  </div>
 
-									<div class="col-2 mx-3">
-										<doughnut-chart />
-										<p class="text-center mt-3 mb-0">By Currency</p>
-									</div>
+                  <div class="col-2 mx-3">
+                    <doughnut-chart/>
+                    <p class="text-center mt-3 mb-0">By Currency</p>
+                  </div>
 
-									<div class="col-2 mx-3">
-										<doughnut-chart />
-										<p class="text-center mt-3 mb-0">By Vintage Year</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+                  <div class="col-2 mx-3">
+                    <doughnut-chart/>
+                    <p class="text-center mt-3 mb-0">By Vintage Year</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<div class="row mb-3">
-				<div class="col">
-					<div class="card border-0 rounded-0">
-						<div class="card-body">
-							<bar-chart />
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</main>
+      <div class="row mb-3">
+        <div class="col">
+          <div class="card border-0 rounded-0">
+            <div class="card-body">
+              <bar-chart/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import DoughnutChart from '@/components/DoughnutChart'
-import BarChart from '@/components/BarChart'
+import DoughnutChart from "@/components/DoughnutChart";
+import BarChart from "@/components/BarChart";
 
 export default {
-	name: "dashboard",
-	components: { 
-		DoughnutChart,
-		BarChart 
-	},
-	created() {
-		console.log('hi')
-	}
-}
+  name: "dashboard",
+  components: {
+    DoughnutChart,
+    BarChart
+  },
+  mounted() {
+    this.$refs.card.focus();
+  }
+};
 </script>
 
 <style scoped>
-.dashboard .stat-cards 
-.card  {
-	border-radius: 0px !important;
-	-webkit-box-shadow: 2px 2px 5px 0px rgba(214,211,214,1);
-	-moz-box-shadow: 2px 2px 5px 0px rgba(214,211,214,1);
-	box-shadow: 2px 2px 5px 0px rgba(214,211,214,1);
+.dashboard .stat-cards .card {
+  border-radius: 0px !important;
+  -webkit-box-shadow: 2px 2px 5px 0px rgba(214, 211, 214, 1);
+  -moz-box-shadow: 2px 2px 5px 0px rgba(214, 211, 214, 1);
+  box-shadow: 2px 2px 5px 0px rgba(214, 211, 214, 1);
 }
 
-.dashboard .stat-cards 
-.card .card-header {
-	background-color: #ffffff !important;
-	border-color: rgba(87, 138, 141, 0.2);
+.dashboard .stat-cards .card .card-header {
+  background-color: #ffffff !important;
+  border-color: rgba(87, 138, 141, 0.2);
 }
 
-.dashboard .stat-cards 
-.card .card-header-title {
-	font-size: 11px !important;
-	font-weight: bold;
-	color: #666666;
+.dashboard .stat-cards .card .card-header-title {
+  font-size: 11px !important;
+  font-weight: bold;
+  color: #666666;
 }
 
-.dashboard .stat-cards 
-.card .card-body {
-	color: #578a8d;
+.dashboard .stat-cards .card .card-body {
+  color: #578a8d;
+}
+
+.dashboard .card:focus {
+  outline: #578a8d solid 1px;
 }
 </style>
 
