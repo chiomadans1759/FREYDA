39e615b273d208584b26bae6e5ca2b7f0dac4343
chiomadans1759 <chiomadans@gmail.com>
2019-05-17 15:17:31 +0100

    Updated the create coverage functionality

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
-import investments from './investment';
+import coverage from "./coverage";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     auth,
-    investments
+    coverage
   }
 });
